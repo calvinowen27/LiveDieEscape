78d8bc31a736adf9d56236214e5a7b9fb373704a
Calvin Owen <calvin.owen27@gmail.com>
2024-09-30 23:31:59 -0700

    getting main set up

---
+++ globals/game.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var _is_game_running = false
+var _is_game_paused = false
+
+func restart_game() -> void:
+	# SceneManager.switch_scene_to("title_screen")
+	_is_game_running = false
+
+# returns new state of game pause
+func toggle_game_paused() -> bool:
+	_is_game_paused = !_is_game_paused
+	get_tree().paused = _is_game_paused
+	return _is_game_paused
+
+func is_game_running() -> bool:
+	return _is_game_running
+
+func _on_game_start() -> void:
+	if not _is_game_running:
+		# switch scene to main
+		_is_game_running = true
