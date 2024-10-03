5e2de428f42c6e1ba0f8bf33d3012d088b3ce238
Calvin Owen <calvin.owen27@gmail.com>
2024-10-03 00:06:38 -0700

    added endurance cooldown to idle state

--- scenes/player/player_idle.gd
+++ scenes/player/player_idle.gd
@@ -10,6 +10,13 @@ func state_init() -> void:
 	super.state_init()
 	_animated_sprite.animation = "player_idle"
 	_animated_sprite.play()
+	
+	if StatManager.get_stat("endurance") < StatManager.get_base_stat("endurance"):
+		$EnduranceCooldownTimer.start()
+
+func disable() -> void:
+	super.disable()
+	$EnduranceCooldownTimer.stop()
 
 func update(delta: float) -> String:
 	var move_vec = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -19,3 +26,8 @@ func update(delta: float) -> String:
 		return "PlayerWalk"
 	
 	return name
+
+func _on_endurance_cooldown_timer_timeout() -> void:
+	StatManager.change_stat("endurance", 1)
+	if StatManager.get_stat("endurance") < StatManager.get_base_stat("endurance"):
+		$EnduranceCooldownTimer.start()
