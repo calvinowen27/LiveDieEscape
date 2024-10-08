a3fce99e3def29b518bbfb6fe7cad9e1a4e61850
Calvin Owen <calvin.owen27@gmail.com>
2024-10-08 01:06:21 -0700

    forgot to override _set_curr_state() for item

--- scenes/items/item_placed.gd
+++ scenes/items/item_placed.gd
@@ -6,7 +6,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func item_state_enable(rigidbody: RigidBody2D, sprite: Sprite2D) -> void:
-	super.item_state_enable(rigidbody, sprite)
+func item_state_enable(item: Item, rigidbody: RigidBody2D, sprite: Sprite2D) -> void:
+	super.item_state_enable(item, rigidbody, sprite)
 
 	rigidbody.sleeping = false
+
+	sprite.texture = item.get_object_texture()
--- scenes/items/item_state.gd
+++ scenes/items/item_state.gd
@@ -2,13 +2,32 @@ extends State
 
 class_name ItemState
 
+var _item: Item
 var _rigidbody: RigidBody2D
 var _sprite: Sprite2D
 
 func _ready() -> void:
 	_set_curr_state("ItemGround")
 
-func item_state_enable(rigidbody: RigidBody2D, sprite: Sprite2D) -> void:
+func _set_curr_state(new_state_name: String) -> State:
+	var new_state = super._set_curr_state(new_state_name)
+	
+	# _curr_state == new_state at this point
+	
+	if new_state == null:
+		return null
+	
+	# enable new state
+	if _curr_state != null:
+		var item = get_parent()
+		var rigidbody = get_parent()
+		var sprite = rigidbody.get_node("Sprite2D")
+		_curr_state.item_state_enable(item, rigidbody, sprite)
+
+	return new_state
+
+func item_state_enable(item: Item, rigidbody: RigidBody2D, sprite: Sprite2D) -> void:
+	_item = item
 	_rigidbody = rigidbody
 	_sprite = sprite
 
