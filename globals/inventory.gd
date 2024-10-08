643d082ab40683313633cd53001ef21ae97f07f5
Calvin Owen <calvin.owen27@gmail.com>
2024-10-07 22:39:23 -0700

    basic inventory interface complete

--- globals/inventory.gd
+++ globals/inventory.gd
@@ -7,14 +7,30 @@ func _ready() -> void:
 	for i in range(_size):
 		_slots.append(null)
 
-func add_item(item: Item) -> bool:
-	var item_added = false
+func add_item(item: Item) -> int:
+	var item_idx = -1
 	for i in range(_size):
 		if _slots[i] == null:
 			_slots[i] = item
-			item_added = true
+			item_idx = i
+			break
 	
-	return item_added
+	# add item to hud
+	# change item state from ground to inventory
+	
+	return item_idx
+
+func drop_item(item_idx: int) -> void:
+	if _slots[item_idx] == null:
+		print_debug("drop_item(): no item in slot %d" % item_idx)
+		return
+	
+	var item = _slots[item_idx]
+	
+	# remove item from hud
+	# change item state from inventory to ground/destroy
+	
+	_slots[item_idx] = null
 
 func size() -> int:
 	return _size
