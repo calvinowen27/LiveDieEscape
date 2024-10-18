efe321b12f5d7407b45f38753a693ac47c484df6
Calvin Owen <calvin.owen27@gmail.com>
2024-10-18 00:35:52 -0700

    removed control button focus stuff

--- scenes/world_objects/control_point.gd
+++ scenes/world_objects/control_point.gd
@@ -73,6 +73,7 @@ func _update_control() -> void:
 		button_info[1] = true
 		button.set("theme_override_colors/font_color", Color(0.0, 1.0, 0.0, 1.0))
 		button.set("theme_override_colors/font_pressed_color", Color(0.0, 1.0, 0.0, 1.0))
+		button.set("theme_override_colors/font_hover_pressed_color", Color(0.0, 1.0, 0.0, 1.0))
 
 		for chip in control_chips:
 			if chip.get_control_level() != _level:
@@ -89,6 +90,7 @@ func _update_control() -> void:
 			button.text = control_button_unavailable_text % control_room
 			button.set("theme_override_colors/font_color", Color(1.0, 0.0, 0.0, 1.0))
 			button.set("theme_override_colors/font_pressed_color", Color(1.0, 0.0, 0.0, 1.0))
+			button.set("theme_override_colors/font_hover_pressed_color", Color(1.0, 0.0, 0.0, 1.0))
 
 func _on_control_button_pressed(room_idx: int) -> void:
 	if RoomManager.get_curr_room_idx() == room_idx:
