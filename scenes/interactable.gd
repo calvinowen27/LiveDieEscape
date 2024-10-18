cd228a7f1d841c2ec2fd05e9f2398db2e8256158
Calvin Owen <calvin.owen27@gmail.com>
2024-10-17 23:56:18 -0700

    circular progress bar added to hold to interact

--- scenes/interactable.gd
+++ scenes/interactable.gd
@@ -9,12 +9,15 @@ var _is_active = true
 var _interacting = false
 
 var _interact_timer: Timer
+var _progress_bar: TextureProgressBar
 
 signal interact
 signal interactable_set(val: bool)
 
 func _ready() -> void:
 	_interact_timer = $InteractTimer
+	_progress_bar = $InteractLabel/TextureProgressBar
+	
 	if _hold_to_interact:
 		$InteractLabel.text = "Hold  E  to Interact"
 
@@ -27,9 +30,13 @@ func _process(_delta: float) -> void:
 			_interact_timer.start()
 			_interacting = true
 	
-	if _interacting and (Input.is_action_just_released("interact") or not _player_in_range):
-		_interact_timer.stop()
-		_interacting = false
+	if _interacting:
+		if (Input.is_action_just_released("interact") or not _player_in_range):
+			_interact_timer.stop()
+			_interacting = false
+			_progress_bar.value = 0
+		else:
+			_progress_bar.value = 100 - int(_interact_timer.time_left * 100)
 
 func _on_interact_area_body_entered(body: Node2D) -> void:
 	if body == RoomManager.get_player() and _is_active:
