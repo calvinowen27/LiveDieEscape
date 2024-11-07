c8bdcc1ea11bf9562f7bffdddee2e36e2b2f3aba
Calvin Owen <calvin.owen27@gmail.com>
2024-11-06 23:28:21 -0800

    fixed control chip item info

--- scenes/items/control_chip.gd
+++ scenes/items/control_chip.gd
@@ -4,7 +4,7 @@ extends Item
 @export var _control_room: int
 
 func _ready() -> void:
-	_item_info = "Level: %d Room: %d" % [_control_level, _control_room]
+	_item_info = "Level: 0 Room: 0"
 
 	super._ready()
 
@@ -13,6 +13,12 @@ func set_control_level(control_level: int) -> void:
 
 func set_control_room(control_room: int) -> void:
 	_control_room = control_room
+
+func set_control(control_level: int, control_room: int) -> void:
+	_control_level = control_level
+	_control_room = control_room
+
+	_item_info = "Level: %d Room: %d" % [_control_level, _control_room]
 
 func get_control_level() -> int:
 	return _control_level
--- scenes/laser_turret/rotating_laser_turret.gd
+++ scenes/laser_turret/rotating_laser_turret.gd
@@ -4,7 +4,6 @@ func _on_interactable_interact() -> void:
 	# spawn control chip
 	if $LaserTurretState.get_curr_state().name == "LaserTurretActivated":
 		var control_chip = RoomManager.instantiate_item("control_chip", position + Vector2(0, 50))
-		control_chip.set_control_level(RoomManager.get_curr_level())
-		control_chip.set_control_room(RoomManager.get_curr_room_idx())
+		control_chip.set_control(RoomManager.get_curr_level(), RoomManager.get_curr_room_idx())
 	
 	$LaserTurretState.disable_turret()
