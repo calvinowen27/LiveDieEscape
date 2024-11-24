283c287b609a49e9b6ec0981c71ad34be4b97cb3
Calvin Owen <calvin.owen27@gmail.com>
2024-11-23 22:31:11 -0800

    made item place script generic

--- scenes/items/item_placed.gd
+++ scenes/items/item_placed.gd
@@ -1,5 +1,8 @@
 extends ItemState
 
+@export var _object_name: String
+@export var _pos_offset: Vector2
+
 func _ready() -> void:
 	pass
 
@@ -9,7 +12,8 @@ func _process(_delta: float) -> void:
 func item_state_enable(item: Item, sprite: Sprite2D) -> void:
 	super.item_state_enable(item, sprite)
 
-	# unlock item position
+	var obj = load("res://scenes/world_objects/%s.tscn" % _object_name).instantiate()
+	RoomManager.get_curr_room().add_child(obj)
+	obj.position = RoomManager.get_player().position + _pos_offset
 
-	sprite.texture = item.get_object_texture()
-	sprite.visible = true
+	Inventory.del_item(get_node("../../") as Item)
