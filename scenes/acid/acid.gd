3a1e8ddd0ce9712620ada0a67dd1c695220bd9ae
Calvin Owen <calvin.owen27@gmail.com>
2024-12-03 22:16:04 -0800

    changed acid animation name

--- scenes/acid/acid.gd
+++ scenes/acid/acid.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 func _ready() -> void:
-	$AnimationPlayer.play("acid")
+	$AnimationPlayer.play("acid_bubble")
 
 func _on_body_entered(body: Node2D) -> void:
 	if body == RoomManager.get_player():
