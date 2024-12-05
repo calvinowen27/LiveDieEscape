0e72b154f32fc22cbf4dacf87f11502f2e014a7d
Calvin Owen <calvin.owen27@gmail.com>
2024-12-04 22:57:14 -0800

    acid edge hold mechanic added

--- scenes/acid/acid.gd
+++ scenes/acid/acid.gd
@@ -1,8 +1,20 @@
-extends Area2D
+extends RigidBody2D
 
 func _ready() -> void:
 	$AnimationPlayer.play("acid_bubble")
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body == RoomManager.get_player():
 		body.die()
+
+func _on_edge_timer_timeout() -> void:
+	$CollisionShape2D.disabled = true
+
+func _on_rigid_body_2d_body_entered(body: Node) -> void:
+	if body == RoomManager.get_player():
+		$EdgeTimer.start()
+
+func _on_rigid_body_2d_body_exited(body: Node) -> void:
+	if body == RoomManager.get_player():
+		$EdgeTimer.stop()
+		$CollisionShape2D.disabled = false
