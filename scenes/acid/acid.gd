283e03d0bc11ae2b0de703cdee6fcb4b4e2b77da
Calvin Owen <calvin.owen27@gmail.com>
2024-12-09 13:11:00 -0800

    fixed annoying acid problem

--- scenes/acid/acid.gd
+++ scenes/acid/acid.gd
@@ -1,20 +1,22 @@
 extends RigidBody2D
 
+var _player_touching = false
+
 func _ready() -> void:
 	$AnimationPlayer.play("acid_bubble")
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body == RoomManager.get_player():
-		body.die()
-
 func _on_edge_timer_timeout() -> void:
-	$CollisionShape2D.disabled = true
+	$CollisionShape2D.set_deferred("disabled", true)
 
 func _on_rigid_body_2d_body_entered(body: Node) -> void:
 	if body == RoomManager.get_player():
 		$EdgeTimer.start()
 
-func _on_rigid_body_2d_body_exited(body: Node) -> void:
+func _on_lethal_area_body_entered(body: Node2D) -> void:
+	if body == RoomManager.get_player():
+		body.die()
+
+func _on_total_area_body_exited(body: Node2D) -> void:
 	if body == RoomManager.get_player():
 		$EdgeTimer.stop()
-		$CollisionShape2D.disabled = false
+		$CollisionShape2D.set_deferred("disabled", false)
