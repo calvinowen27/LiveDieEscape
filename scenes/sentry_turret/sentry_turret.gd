229d2c83a9d114da6da46cc8424bdb04b520eb9d
Calvin Owen <calvin.owen27@gmail.com>
2024-12-21 23:39:37 -0800

    getting sentry turret directional shooting working

--- scenes/sentry_turret/sentry_turret.gd
+++ scenes/sentry_turret/sentry_turret.gd
@@ -1,6 +1,16 @@
 extends RigidBody2D
 
-var _projectile = preload("res://scenes/sentry_turret/projectile.tscn")
+const _projectile = preload("res://scenes/sentry_turret/projectile.tscn")
+
+@export var _track_player: bool = true
+@export var _setup_rotation: int = 0
+
+func _ready() -> void:
+	$Sprite2D.frame_coords = Vector2i(int((float(_setup_rotation) / 360) * 4), 0)
+
+	var rotation_rads = _setup_rotation * PI / 180
+	$ProjectileSpawn.position = Vector2(-cos(rotation_rads) * 135, -81 + sin(rotation_rads) * 115)
+	print($ProjectileSpawn.position)
 
 func _on_shoot_cooldown_timer_timeout() -> void:
 	shoot()
@@ -11,6 +21,13 @@ func shoot() -> void:
 	
 	proj.global_position = $ProjectileSpawn.global_position
 
-	var lin_vel = (RoomManager.get_player().global_position - $ProjectileSpawn.global_position).normalized() * 500
+	var dir
+	if _track_player:
+		dir = (RoomManager.get_player().global_position - $ProjectileSpawn.global_position).normalized()
+	else:
+		var rotation_rads = _setup_rotation * PI / 180
+		dir = Vector2(-cos(rotation_rads), sin(rotation_rads))
+
+	var lin_vel = dir * 500
 
 	proj.init_projectile(self, lin_vel)
