a0607d4fbe2e8709a8e72475448d8050380ef2de
Calvin Owen <calvin.owen27@gmail.com>
2024-12-22 20:52:38 -0800

    remove deprecated code

--- scenes/player/player_state.gd
+++ scenes/player/player_state.gd
@@ -44,13 +44,6 @@ func player_state_enable(sprite: Sprite2D, rigidbody: RigidBody2D, animation_pla
 	
 	super.enable()
 
-# flip sprite depending on move direction, retain last direction
-# func point_sprite() -> void:
-# 	if _move_dir.x > 0:
-# 		_sprite.scale.x = -1 * abs(_sprite.scale.x)
-# 	elif _move_dir.x < 0:
-# 		_sprite.scale.x = 1 * abs(_sprite.scale.x)
-
 # move player based on input via rigidbody linear velocity
 # also update last_move_dir for dashing from idle state
 func move() -> void:
