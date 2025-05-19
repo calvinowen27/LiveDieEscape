f96096a67ed30017e200d294e23bf5a505724559
Calvin Owen <calvin.owen27@gmail.com>
2025-05-19 13:23:10 -0700

    deleted powerups

--- scenes/items/speed_powerup.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-func _on_body_entered(body:Node2D) -> void:
-	var player = RoomManager.get_player()
-	if body == player:
-		self.queue_free()
-
-		PowerupManager.add_powerup("speed")
