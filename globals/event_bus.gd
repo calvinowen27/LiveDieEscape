bfb5b871b7762f1da883c9912563fb930ef6f210
Calvin Owen <calvin.owen27@gmail.com>
2025-05-19 13:36:07 -0700

    deleting old rooms and powerup stuff

--- globals/event_bus.gd
+++ globals/event_bus.gd
@@ -6,7 +6,5 @@ signal item_pickup(item: Item)
 signal level_reset(level_idx: int)
 signal player_death
 signal player_respawn
-signal powerup_pickup(powerup_name: String)
-signal powerup_over(powerup_name: String)
 signal materials_update
 signal recipe_select(recipe: RecipeCell)
