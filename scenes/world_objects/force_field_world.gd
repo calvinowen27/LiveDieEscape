1c9041482261373b44d9e70289044e06e3778e58
Calvin Owen <calvin.owen27@gmail.com>
2025-05-27 18:36:36 -0700

    experimenting with impenetrable force field and size

--- scenes/world_objects/force_field_world.gd
+++ scenes/world_objects/force_field_world.gd
@@ -1,12 +1,22 @@
-extends Area2D
+extends Node2D
+
+class_name ForceField
 
 @export var _temporary: bool = true
+@export var _penetrable: bool = true
+
+func _ready() -> void:
+	if _penetrable:
+		$RigidBody2D.set_collision_layer_value(1, false)
 
 func _on_timer_timeout() -> void:
 	if _temporary:
 		self.queue_free()
 
-func _on_body_entered(body: Node2D) -> void:
+func _handle_projectile(body: Node2D) -> void:
 	# basically if it's a projectile kill it (can't do this through the projectile rigidbody for some reason)
 	if (body as Projectile) != null:
 		body.queue_free()
+
+func _on_area_2d_body_entered(body:Node2D) -> void:
+	_handle_projectile(body)
