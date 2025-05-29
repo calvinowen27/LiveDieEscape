a7473a352ccbd0b0546231be2b8c6f7763e72788
Calvin Owen <calvin.owen27@gmail.com>
2025-05-28 18:04:51 -0700

    improved disruptor code

--- scenes/world_objects/disruptor_world.gd
+++ scenes/world_objects/disruptor_world.gd
@@ -5,23 +5,32 @@ class_name Disruptor
 var _disrupting: Array[Disruptable]
 
 func die() -> void:
+	# stop disrupting everything and then destroy self
 	for disruptable in _disrupting:
 		disruptable.end_disrupt(self)
 	
 	self.queue_free()
 
+# start disrupting anything in range
 func _on_detection_area_body_entered(body: Node2D) -> void:
-	if body.has_node("Disruptable"):
-		var disruptable = body.get_node("Disruptable")
-		if disruptable in _disrupting: return
+	if not body.has_node("Disruptable"): return
 
-		disruptable.disrupt(self)
-		_disrupting.append(disruptable)
+	var disruptable = body.get_node("Disruptable")
 
+	# only if not already disrupting
+	if disruptable in _disrupting: return
+
+	disruptable.disrupt(self)
+	_disrupting.append(disruptable)
+
+# stop disrupting if out of range
 func _on_detection_area_body_exited(body: Node2D) -> void:
-	if body.has_node("Disruptable"):
-		var disruptable = body.get_node("Disruptable")
-		if disruptable not in _disrupting: return
+	if not body.has_node("Disruptable"): return
 
-		disruptable.end_disrupt(self)
-		_disrupting.erase(disruptable)
+	var disruptable = body.get_node("Disruptable")
+
+	# only if currently disrupting
+	if disruptable not in _disrupting: return
+
+	disruptable.end_disrupt(self)
+	_disrupting.erase(disruptable)
