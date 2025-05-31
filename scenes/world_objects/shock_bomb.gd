d1e099ac086db9454b0d05a1d9848c96a89e3e98
Calvin Owen <calvin.owen27@gmail.com>
2025-05-30 20:47:24 -0700

    made shock bomb shock player and made it stay after shock over

--- scenes/world_objects/shock_bomb.gd
+++ scenes/world_objects/shock_bomb.gd
@@ -14,16 +14,16 @@ func _process(_delta: float) -> void:
 	if _detonating:
 		# shock all nodes in range (if not shocked already)
 		for node in _nodes_in_range:
-			if node is LaserTurret:
-				node.shock()
+			if node.has_node("Shockable"):
+				node.get_node("Shockable").shock()
 		_nodes_in_range.clear()
 
 # TODO: note to self, add placing object "thunk" kinda sound
 
 func _on_alive_timer_timeout() -> void:
-	# _detonating = false
-	# _animation_player.play("RESET")
-	self.queue_free()
+	_detonating = false
+	_animation_player.play("RESET")
+	# self.queue_free()
 
 func detonate() -> void:
 	_detonating = true
